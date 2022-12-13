cf58a01328a436a09b22880f6bb07d7eb1341228
Nathaniel Quansah <n.bongo40@gmail.com>
2022-12-13 18:09:28 +0000

    update

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 // express
 import express from "express";
+// imports
+import SwaggerUi from 'swagger-ui-express';
 
 // intialize express app
 const app = express();
+
+// swagger
+import swaggerDocument from './swagger/swagger.json';
 
 // routes
 import incidentsRoute from "./routes/incidents.route";
@@ -15,6 +20,14 @@ app.use(express.json());
 
 // use routes
 app.use("/incidents", incidentsRoute);
+
+// use swagger
+app.use('/swagger', SwaggerUi.serve, SwaggerUi.setup(swaggerDocument));
+
+// default route
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 // start the Express server
 const server = app.listen(serverPort, () =>
